3f3bb5ee758a0140b3119c7fa1a1c81149dc9e44
Rahul <rahulmkhj@gmail.com>
2021-06-16 12:39:28 +0530

    removed default path registration for console commands
    Modules will register commands in their respective service provider classes

--- Modules/Core/Kernel/Console.php
+++ Modules/Core/Kernel/Console.php
@@ -36,8 +36,6 @@ class Console extends Kernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
     }
 }
