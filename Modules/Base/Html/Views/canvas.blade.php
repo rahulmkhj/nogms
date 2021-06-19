9a800a3483701f45199ea08785599d5939e8cd62
Rahul <rahulmkhj@gmail.com>
2021-06-19 16:17:51 +0530

    added base:canvas view
    this will be a bare minimum view only with top & side navbars, all authenticated pages will extend from this including base layouts for various modules

---
+++ Modules/Base/Html/Views/canvas.blade.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>NOGMS - A NOG Management System</title>
+    <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 11]>
+    	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="Rahul Makhija">
+
+    <!-- font css -->
+    <link rel="stylesheet" href="{{asset('assets/fonts/feather.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/fonts/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/fonts/material.css')}}">
+
+    <!-- vendor css -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" id="main-style-link">
+
+</head>
+
+<body class="">
+	<!-- [ Pre-loader ] start -->
+	<div class="loader-bg">
+		<div class="loader-track">
+			<div class="loader-fill"></div>
+		</div>
+	</div>
+	<!-- [ Pre-loader ] End -->
+	<!-- [ Mobile header ] start -->
+	<div class="pc-mob-header pc-header">
+		<div class="pcm-logo">
+			<img src="{{asset('assets/images/nogms-logo-vertical.svg')}}" alt="" class="logo logo-lg">
+		</div>
+		<div class="pcm-toolbar">
+			<a href="#!" class="pc-head-link" id="mobile-collapse">
+				<div class="hamburger hamburger--arrowturn">
+					<div class="hamburger-box">
+						<div class="hamburger-inner"></div>
+					</div>
+				</div>
+			</a>
+			<a href="#!" class="pc-head-link" id="headerdrp-collapse">
+				<i data-feather="align-right"></i>
+			</a>
+			<a href="#!" class="pc-head-link" id="header-collapse">
+				<i data-feather="more-vertical"></i>
+			</a>
+		</div>
+	</div>
+	<!-- [ Mobile header ] End -->
+
+	<!-- [ navigation menu ] start -->
+	<nav class="pc-sidebar ">
+		<div class="navbar-wrapper">
+			<div class="m-header">
+				<a href="{{url('/')}}" class="b-brand">
+					<!-- ========   change your logo hear   ============ -->
+					<img src="{{asset('assets/images/nogms-logo-vertical.svg')}}" alt="" class="logo logo-lg img-fluid">
+					<img src="{{asset('assets/images/nogms-logo-sm.svg')}}" alt="" class="logo logo-sm">
+				</a>
+			</div>
+			<div class="navbar-content">
+				<ul class="pc-navbar">
+{{--					<li class="pc-item pc-caption">--}}
+{{--						<label>Navigation</label>--}}
+{{--					</li>--}}
+{{--					<li class="pc-item">--}}
+{{--						<a href="index.html" class="pc-link "><span class="pc-micon"><i class="material-icons-two-tone">home</i></span><span class="pc-mtext">Dashboard</span></a>--}}
+{{--					</li>--}}
+					<li class="pc-item pc-caption">
+						<label>Programm Committee</label>
+{{--						<span>UI Components</span>--}}
+					</li>
+
+					<li class="pc-item active">
+						<a href="" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Tasks</span>
+                        </a>
+					</li>
+                    <li class="pc-item">
+						<a href="" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Polls</span>
+                        </a>
+					</li>
+                    <li class="pc-item">
+						<a href="" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Meetings</span>
+                        </a>
+					</li>
+                    <li class="pc-item">
+						<a href="" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Presentations</span>
+                        </a>
+					</li>
+                    <li class="pc-item">
+						<a href="" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Workshops</span>
+                        </a>
+					</li>
+
+					<li class="pc-item pc-caption">
+						<label>Admin</label>
+					</li>
+					<li class="pc-item">
+						<a href="chart-apex.html" class="pc-link "><span class="pc-micon"><i class="material-icons-two-tone">bubble_chart</i></span><span class="pc-mtext">Settings</span></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!-- [ navigation menu ] end -->
+	<!-- [ Header ] start -->
+	<header class="pc-header ">
+		<div class="header-wrapper">
+			<div class="mr-auto pc-mob-drp">
+				<ul class="list-unstyled">
+					<li class="dropdown pc-h-item">
+						<a class="pc-head-link active dropdown-toggle arrow-none mr-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+							Program Committee
+						</a>
+						<div class="dropdown-menu pc-h-dropdown">
+							<a href="#!" class="dropdown-item">
+{{--								<i class="material-icons-two-tone">account_circle</i>--}}
+								<span>Fellowship Committee</span>
+							</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="ml-auto">
+				<ul class="list-unstyled">
+					<li class="dropdown pc-h-item">
+						<a class="pc-head-link dropdown-toggle arrow-none mr-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+							<i class="material-icons-two-tone">search</i>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right pc-h-dropdown drp-search">
+							<form class="px-3">
+								<div class="form-group mb-0 d-flex align-items-center">
+									<i data-feather="search"></i>
+									<input type="search" class="form-control border-0 shadow-none" placeholder="Search here. . .">
+								</div>
+							</form>
+						</div>
+					</li>
+					<li class="dropdown pc-h-item">
+						<a class="pc-head-link dropdown-toggle arrow-none mr-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+							<img src="assets/images/user/avatar-2.jpg" alt="user-image" class="user-avtar">
+							<span>
+								<span class="user-name">Joseph William</span>
+								<span class="user-desc">Administrator</span>
+							</span>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right pc-h-dropdown">
+							<a href="" class="dropdown-item">
+								<i class="material-icons-two-tone">exit_to_app</i>
+								<span>Logout</span>
+							</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+
+		</div>
+	</header>
+	<!-- [ Header ] end -->
+
+<!-- [ Main Content ] start -->
+<div class="pc-container">
+    <div class="pcoded-content">
+
+        @yield('content')
+
+
+    </div>
+</div>
+<!-- [ Main Content ] end -->
+    <!-- Warning Section start -->
+    <!-- Older IE warning message -->
+    <!--[if lt IE 11]>
+        <div class="ie-warning">
+            <h1>Warning!!</h1>
+            <p>You are using an outdated version of Internet Explorer, please upgrade
+               <br/>to any of the following web browsers to access this website.
+            </p>
+            <div class="iew-container">
+                <ul class="iew-download">
+                    <li>
+                        <a href="http://www.google.com/chrome/">
+                            <img src="assets/images/browser/chrome.png" alt="Chrome">
+                            <div>Chrome</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.mozilla.org/en-US/firefox/new/">
+                            <img src="assets/images/browser/firefox.png" alt="Firefox">
+                            <div>Firefox</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.opera.com">
+                            <img src="assets/images/browser/opera.png" alt="Opera">
+                            <div>Opera</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.apple.com/safari/">
+                            <img src="assets/images/browser/safari.png" alt="Safari">
+                            <div>Safari</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                            <img src="assets/images/browser/ie.png" alt="">
+                            <div>IE (11 & above)</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <p>Sorry for the inconvenience!</p>
+        </div>
+    <![endif]-->
+    <!-- Warning Section Ends -->
+    <!-- Required Js -->
+    <script src="{{asset('assets/js/vendor-all.min.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/feather.min.js')}}"></script>
+    <script src="{{asset('assets/js/pcoded.min.js')}}"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script> -->
+    <!-- <script src="assets/js/plugins/clipboard.min.js"></script> -->
+    <!-- <script src="assets/js/uikit.min.js"></script> -->
+</body>
+
+</html>
