da33cfea0595178e77f82ce69b8ec1368cb80e4c
Rahul <rahulmkhj@gmail.com>
2021-06-20 08:25:55 +0530

    added favicons

--- Modules/Base/Html/Views/canvas.blade.php
+++ Modules/Base/Html/Views/canvas.blade.php
@@ -15,6 +15,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="author" content="Rahul Makhija">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/images/favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/images/favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('assets/images/favicon/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{asset('assets/images/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- font css -->
     <link rel="stylesheet" href="{{asset('assets/fonts/feather.css')}}">
     <link rel="stylesheet" href="{{asset('assets/fonts/fontawesome.css')}}">
