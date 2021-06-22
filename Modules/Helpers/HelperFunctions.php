8c3a02d7eb4bda1895d014cfb2c5d53a049ee652
Rahul <rahulmkhj@gmail.com>
2021-06-22 19:35:37 +0530

    added modules_path() helper function

--- Modules/Helpers/HelperFunctions.php
+++ Modules/Helpers/HelperFunctions.php
@@ -1,1 +1,14 @@
 <?php
+
+/**
+ * @param string $path
+ * @return string
+ *
+ * This function receives a path parameter relative to Modules directory, and
+ * produces absolute path for the same. And provides absolute path upto
+ * Modules directory when executed without any parameter.
+ */
+function modules_path(string $path = ''): string
+{
+    return app()->basePath("Modules" . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR));
+}
