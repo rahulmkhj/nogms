727da7c6431da8e9b6cfe98bd8fbd0fe44c174be
Rahul <rahulmkhj@gmail.com>
2021-08-19 16:27:52 +0530

    registered index method for Tasks module

--- Modules/Tasks/Routes/web.php
+++ Modules/Tasks/Routes/web.php
@@ -1,1 +1,11 @@
 <?php
+use \Illuminate\Support\Facades\Route;
+use NOGMS\Tasks\Http\Controllers\TasksController;
+
+Route::group([
+    'prefix'    =>  'tasks',
+], function(){
+
+    Route::get('/', [TasksController::class, 'index'])->name('tasks');
+
+});
